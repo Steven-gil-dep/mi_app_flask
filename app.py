e41b92fd754627693a1db9395d862ce7b7878148
Steven Gil <130931260+Steven-gil-dep@users.noreply.github.com>
2025-12-03 07:23:23 -0500

    Refactor app.py for Flask application setup
    salto de linea en alistamiento

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ REQUERIMIENTOS = {
     6: ("Revisión por retiro",
         "Se realizó la revisión del equipo por motivo de retiro. realizando pruebas de componentes de hardware como Teclado, Touchpad, pantalla, Puertos, Càmara, Microfono y Speaker, sin evidenciar anomalìas en su funcionamiento durante las pruebas realizadas, se realiza borrado seguro y eliminacion del equipo de Dominio"),
     7: ("Alistamiento",
-        "Se realizó el alistamiento completo del equipo con cargador /nen buen estado físico y funcionales, se adjuntan datos de la máquina."),
+        "Se realizó el alistamiento completo del equipo con cargador \nen buen estado físico y funcionales, se adjuntan datos de la máquina."),
 }
 
 INCIDENTES = {
@@ -96,5 +96,6 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
